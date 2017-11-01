e2a8d6f19d713648ff5ec684c6b7c4df7aa7d45f
Mike Solomon <msolomon@thrillist.com>
2017-10-31 21:27:00 -0400

    use dirname for heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(fileUpload());
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.post('/classify', function(req, res) {
 
